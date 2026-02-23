b97ab0658c36f63e3f85add840b4714ed5ddc8f7
preisslergo-maker <preissler.go@gmail.com>
2026-02-23 04:26:23 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,45 @@
-const CACHE_NAME = 'happypet-v1';
+const CACHE_NAME = 'happypet-v2'; // Mudei para v2 para o iPhone entender que é novo
+
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/home.html',
-  '/agenda.html',
-  '/clientes.html',
-  '/style.css',
-  '/js/firebase-config.js',
-  '/manifest.json',
-  '/assets/icon-192.png',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+  './',
+  './index.html',
+  './home.html',
+  './agenda.html',
+  './atendimento.html',
+  './clientes.html',
+  './consulta.html',
+  './documento.html',
+  './exame.html',
+  './financeiro.html',
+  './novo_agendamento.html',
+  './novo_cliente.html',
+  './perfil_cliente.html',
+  './pets.html',
+  './vacinas.html',
+  './style.css',
+  './manifest.json',
+  './js/firebase-config.js',
+  './js/client-service.js',
+  './js/novo_cliente_controller.js',
+  './js/utils.js',
+  './assets/icon-192.png',
+  './assets/receita_bg.jpg',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js',
+  'https://www.gstatic.com/firebasejs/8.10.0/firebase-auth.js',
+  'https://www.gstatic.com/firebasejs/8.10.0/firebase-firestore.js'
 ];
 
-// Instalação: Salva os arquivos no cache do iPhone
+// Instalação: Guarda tudo no iPhone
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('Caching assets...');
       return cache.addAll(ASSETS);
     })
   );
 });
 
-// Ativação: Limpa caches antigos
+// Ativação: Remove lixo antigo
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
@@ -33,7 +50,7 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-// Estratégia: Tenta carregar do Cache primeiro, se não tiver, vai na Rede
+// Estratégia: Cache primeiro (Velocidade Máxima)
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
